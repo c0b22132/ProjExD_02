dda077bdd84c44f2cb8fda49d92c0d93f142e603
c0b221327d <c0b221327d@edu.teu.ac.jp>
2023-04-25 14:19:49 +0900

    練習問題２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
-import pygame as pg
+import random
 import sys
 
+import pygame as pg
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -9,8 +10,10 @@ def main():
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     bb_img=pg.Surface((20,20))
-    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
-    bb_img.set_colorkey((0,0,0))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)  # 練習１circleの描画設定
+    bb_img.set_colorkey((0,0,0))  # 練習１Surfaceの透明化
+    x,y=random.randint(0,1600),random.randint(0,900)  # 練習２ランダムな座標の生成
+    screen.blit(bb_img,[x,y])
     tmr = 0
 
     while True:
@@ -21,7 +24,7 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
-
+        screen.blit(bb_img,[x,y])
         pg.display.update()
         clock.tick(1000)
 
